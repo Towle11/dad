d87e182a430e0cc5f02f57a730982ec116d1bb01
Abdi shakur omar <75699810+Towle11@users.noreply.github.com>
2021-05-01 22:46:41 +0300

    Create Mom.py

---
+++ Mom.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python2
+#coding=utf-8
+use="mama"
+pas="towle"
+Somaila = "Muqdisho"
+Kenya = "Niroba"
+eth= "Ardisbaba"
+from time import sleep
+def start():
+	name ="shakur"
+        
+	print("\t\ti i i")
+	sleep(1)
+	print("\t\ti i i")
+	sleep(1)
+	print("\t:_________________:")
+	print("\t:       Happy   ++++        :")
+	sleep(1)
+	print("\t:    ++++       Birthday      | :")
+	sleep(1) 
+	print("\t/______________________________\  ")
+	sleep(1)
+	print("\t:-:-:-:-:-:-:-:-:-:-:-:-:-:-")
+	sleep(.25)
+	print("\t:-:-:-:-:-:-:-:-:-:-:-:-:-:-")
+	sleep(.25)
+	print("\t:-:-:-:-:-:-:-:-:-:-:-:-:-:-")
+	sleep(.25)
+	print("\t__________________________")
+	sleep( .25)
+	print("\t | |    ||   ||     ||     ||  ")
+	sleep(.25)
+	print("\t | |    ||   ||     ||     ||")
+	sleep(.25)
+	print("\t | |    ||   ||     ||     ||")
+	sleep(.25)  
+	print("\t | |    ||   ||     ||     ||")
+	
+	print("\t | |    ||   ||     ||     ||  ")
+	sleep(.25)
+	print("\t | |    ||   ||     ||     ||")
+	sleep(5)
+	print("\t_____________________________")
+	sleep(5)
+	print("Loading.........")
+	sleep(1)
+	print("A massege to You........")
+	sleep(4)
+	print("\t MAY GOD BLESS YOU!! :)")
+	print("TO DAY IS YOUR DAY MY BRO TOWLE:")
+a=input("Enter your key :")
+if a =="win":
+	start()
+username = input("Enter Username:")
+password= input("Enter Password:")
+if username==use:
+	print()
+else:
+	print("wrong")
+if password ==pas:
+	print("1.Somalia:")
+	print("2.Kenya:")
+	print("3.Ethopia:")
+else:
+	print("That is not regestrited this Number:")
+h=input("Enter the County:")
+if h=="1":
+		print("1.CAPITAL CITY IS :",Somaila)
+		print("2.CODE IS: 252")
+		w=input("Enter your chois:")
+		if w=="1":
+			print("The peaple of Somalia Is 31M:")
+		if w=="2":
+			print("The Comoceniction In somalia is Hormuud:")
+elif h=="2":
+		print("1.CAPITAL CITY IS :",Kenya)
+		print("2.CODE IS :254")
+		e=input("Enter Your Chois")
+		if e=="1":
+			print("The people of Kenya is 29M:")
+			
+elif h=="3":
+		print("1.CAPITAL CITY IS :",eth)
+		print("2.CODE IS: 253")
+else:
+			print("invalid code")
+			print("That is not regestrited this Number:")
+
